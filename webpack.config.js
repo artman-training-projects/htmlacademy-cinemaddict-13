04bee3f124db5c19e4e8bd5a34a30c19651fa060
Vladimir  Artyukhov <ya.artriches@ya.ru>
2020-12-06 21:14:23 +0300

    Create test-and-build.yml
    actions: add test & deploy

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, './public'),
   },
   devtool: 'source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
+    contentBase: path.resolve(__dirname, './public'),
     watchContentBase: true,
   }
 };
